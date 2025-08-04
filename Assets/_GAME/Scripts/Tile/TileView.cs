d1054c9d9fc49c6ec369b53966aa196f4acac40e
lamtrinhh1201 <lamtrinhh1201@gmail.com>
2025-08-04 15:48:43 +0700

    change sprites

--- Assets/_GAME/Scripts/Tile/TileView.cs
+++ Assets/_GAME/Scripts/Tile/TileView.cs
@@ -8,31 +8,17 @@ namespace _GAME.Scripts.Tile
         public TileType   Type         { get; set; }
         public Vector2Int GridPosition { get; set; }
 
-        [SerializeField] private Animator _animator;
+        [SerializeField] private Animator   _animator;
         [SerializeField] private GameObject _highlight;
 
         private void OnEnable()
         {
-            TileManager.OnTileClicked += HandleTileClicked;
             _highlight.SetActive(false);
-        }
-
-        private void OnDisable()
-        {
-            TileManager.OnTileClicked -= HandleTileClicked;
-        }
-
-        private void HandleTileClicked(TileView chosenTile)
-        {
-            if (chosenTile == this)
-            {
-                this._animator.Play("Clicked");
-            }
         }
 
         public void SetHighlight(bool highlight)
         {
-           _highlight.SetActive(highlight);
+            _highlight.SetActive(highlight);
         }
     }
 }
