2893ed2927db83e7d0bb916873a669968423780d
lamtrinhh1201 <lamtrinhh1201@gmail.com>
2025-08-04 14:18:20 +0700

    start implement bot logic

---
+++ Assets/_GAME/Scripts/Core/GameManager.cs
@@ -0,0 +1,72 @@
+// File: GameManager.cs
+using System;
+using _GAME.Scripts.Core;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public static event Action<GameState> OnGameStateChanged;
+
+    private GameState _currentState;
+    public GameState CurrentState
+    {
+        get => _currentState;
+        private set
+        {
+            if (_currentState != value)
+            {
+                _currentState = value;
+                OnGameStateChanged?.Invoke(_currentState);
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        UpdateGameState(GameState.MainMenu);
+    }
+
+    public void UpdateGameState(GameState newState)
+    {
+        CurrentState = newState;
+
+        switch (newState)
+        {
+            case GameState.MainMenu:
+                Debug.Log("Game State: Main Menu");
+                break;
+            case GameState.LevelSetup:
+                Debug.Log("Game State: Level Setup");
+                break;
+            case GameState.Playing:
+                Debug.Log("Game State: Playing");
+                Time.timeScale = 1f;
+                break;
+            case GameState.Paused:
+                Debug.Log("Game State: Paused");
+                Time.timeScale = 0f;
+                break;
+            case GameState.LevelComplete:
+                Debug.Log("Game State: Level Complete");
+                break;
+            case GameState.GameOver:
+                Debug.Log("Game State: Game Over");
+                break;
+        }
+    }
+}
---
+++ Assets/_GAME/Scripts/Core/GameState.cs
@@ -0,0 +1,12 @@
+namespace _GAME.Scripts.Core
+{
+    public enum GameState
+    {
+        MainMenu = 0,
+        LevelSetup = 1,
+        Playing = 2,
+        Paused = 3,
+        LevelComplete = 4,
+        GameOver = 5,
+    }
+}
