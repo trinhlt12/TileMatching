650bac4eb90c57e404004a05bf7e517f2b277207
lamtrinhh1201 <lamtrinhh1201@gmail.com>
2025-08-05 15:34:51 +0700

    add select effect

--- Assets/_GAME/Scripts/Tile/TileView.cs
+++ Assets/_GAME/Scripts/Tile/TileView.cs
@@ -16,6 +16,13 @@ namespace _GAME.Scripts.Tile
         [SerializeField] private Animator   _animator;
         [SerializeField] private GameObject _highlight;
 
+        [Header("Highlight Effect Settings")]
+        [SerializeField] private Vector3 punchScale      = new Vector3(0.2f, 0.2f, 0f);
+        [SerializeField] private float punchDuration   = 0.4f;
+        [SerializeField] private int   punchVibrato    = 5;
+        [SerializeField] private float punchElasticity = 0.5f;
+        private                  Tween _highlightTween;
+
         private void OnEnable()
         {
             _highlight.SetActive(false);
@@ -28,7 +35,23 @@ namespace _GAME.Scripts.Tile
 
         public void SetHighlight(bool highlight)
         {
-            _highlight.SetActive(highlight);
+            _highlightTween?.Kill();
+            if (highlight)
+            {
+                _highlight.SetActive(true);
+
+                this._highlightTween = this.TileVisual.transform.DOPunchScale(
+                    this.punchScale,
+                    this.punchDuration,
+                    this.punchVibrato,
+                    this.punchElasticity);
+
+            }
+            else
+            {
+                _highlight.SetActive(false);
+                TileVisual.transform.localScale = Vector3.one;
+            }
         }
 
         public void AnimateSpawn(float delay)
